74567361724d39b9328c5c766e34ec2a1d3954aa
Tushar Kumar <tr.jakhara019@gmail.com>
2025-05-23 03:12:57 +0530

    task status color coded

--- src/components/ui/TaskCard.jsx
+++ src/components/ui/TaskCard.jsx
@@ -6,15 +6,17 @@ import { useNavigate } from 'react-router-dom'
 
 const TaskCard = ({task, onEdit, onDelete, handleClick}) => {
 
+  const taskStatusColor =  task.status==='Completed' ? 'bg-green-200' : task.status==='In Progress' ? 'bg-blue-200' : 'bg-red-200'
+
   return (
-    <tr key={task.id} className='bg-gray-100 my-4 hover:bg-gray-200 cursor-pointer' onClick={()=>{handleClick()}}>
+    <tr key={task.id} className='bg-gray-100 border-b-8 border-white hover:bg-gray-200 cursor-pointer' onClick={()=>{handleClick()}}>
         <td className='py-2 px-4 text-left'>{task.name.slice(0, 20)}</td>
-        <td className='py-2 px-4'>{task.status}</td>
-        <td className='py-2 px-4'>{task.description?.slice(0, 20)}</td>
-        <td className='py-2 px-4'>{task.due_date}</td>
-        <td className='py-2 px-4'>{task.users.map(user=>(user.user_name)).join(', ')}</td>
-        <td className='py-2 px-4'>{task.updated_at}</td>
-        <td className='py-2 px-4'>{task.created_at}</td>
+        <td className='py-2 px-4'><p className={`px-2 py-1 rounded-lg ${taskStatusColor}`}>{task.status}</p></td>
+        <td className='py-2 px-4'><p className='px-2 py-1'>{task.description?.slice(0, 20)}</p></td>
+        <td className='py-2 px-4'><p className='px-2 py-1'>{task.due_date}</p></td>
+        <td className='py-2 px-4'><p className='px-2 py-1'>{task.users.map(user=>(user.user_name)).join(', ')}</p></td>
+        <td className='py-2 px-4'><p className='px-2 py-1'>{task.updated_at}</p></td>
+        <td className='py-2 px-4'><p className='px-2 py-1'>{task.created_at}</p></td>
     </tr>
   )
 }
