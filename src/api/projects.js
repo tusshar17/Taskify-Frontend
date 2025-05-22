85d911abe0cd3d5aa7146f78052868e54aa40b9a
Tushar Kumar <tr.jakhara019@gmail.com>
2025-05-23 04:33:32 +0530

    defaul sorting order changed

--- src/api/projects.js
+++ src/api/projects.js
@@ -5,7 +5,7 @@ const projectsAPI = axios.create({
     baseURL: `${base_api}/project`
 })
 
-export const getProjects = (page=1, per_page=6, sort_by='updated_at') => projectsAPI.get(`/?page=${page}&per_page=${per_page}&sort_by=${sort_by}&order=asc`)
+export const getProjects = (page=1, per_page=6, sort_by='updated_at') => projectsAPI.get(`/?page=${page}&per_page=${per_page}&sort_by=${sort_by}&order=desc`)
 export const getProjectById = (id) => projectsAPI.get(`/${id}`)
 export const createProject = (project) => projectsAPI.post('/', project)
 export const updateProject = (id, project) => projectsAPI.put(`/${id}`, project)
--- src/api/users.js
+++ src/api/users.js
@@ -5,7 +5,7 @@ const userAPI = axios.create({
     baseURL: `${base_api}/user`
 })
 
-export const getUsers = (page=1, per_page=6, sort_by='updated_at') => userAPI.get(`/?page=${page}&per_page=${per_page}&sort_by=${sort_by}&order=asc`)
+export const getUsers = (page=1, per_page=6, sort_by='updated_at') => userAPI.get(`/?page=${page}&per_page=${per_page}&sort_by=${sort_by}&order=desc`)
 export const createUser = (user) => userAPI.post('/', user)
 export const updateUser = (id, user) => userAPI.put(`/${id}`, user)
 export const deleteUser = (id) => userAPI.delete(`/${id}`)
